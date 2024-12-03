26142caf867248398ef520d97379f9b4b1b06129
bryceberczik <berczikbryce@gmail.com>
2024-12-03 15:06:43 -0600

    fixed overload bug on deleteUser

--- server/src/controllers/user-controller.ts
+++ server/src/controllers/user-controller.ts
@@ -68,7 +68,7 @@ export const updateUser = async (req: Request, res: Response) => {
 
       res.status(200).json(updatedUser);
     } else {
-      res.status(404).json({ message: "User not found" });
+      res.status(404).json({ message: 'User not found' });
     }
   } catch (error: any) {
     res.status(500).json({ message: error.message });
@@ -85,17 +85,16 @@ export const deleteUser = async (req: Request, res: Response) => {
       },
     });
 
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
+    if (user) {
+      await prisma.user.delete({
+        where: {
+          id: id,
+        },
+      });
+      res.status(200).json({ message: 'User deleted successfully' });
+    } else {
+      res.status(404).json({ message: 'User not found' });
     }
-
-    await prisma.user.delete({
-      where: {
-        id: id,
-      },
-    });
-
-    res.json({ message: "User deleted" });
   } catch (error: any) {
     res.status(500).json({ message: error.message });
   }
