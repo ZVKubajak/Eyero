a5e85d04927fec176bb5a3761a101eacf17e9c81
Zander <zvkubajak@gmail.com>
2024-12-04 12:13:25 -0600

    Revert "Feature: Stock API Setup"

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,11 @@
-import { Router } from "express";
-import stockRoutes from "./api/stockRoutes";
-import userRouter from "./user-routes/index";
+import { Router } from 'express';
+import apiRouter from "./api/index.js";
+import userRouter from "./user-routes/index.js";
 
 const router = Router();
 
-router.use("/stocks", stockRoutes);
-router.use("/users", userRouter);
+router.use("/api", apiRouter);
+
+router.use("/user-routes", userRouter);
 
 export default router;
