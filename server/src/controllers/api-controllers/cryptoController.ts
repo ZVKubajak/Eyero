9edb6babbc32e40ba7ef1fa47ff84c9696732b7a
VanceGoblin <zvkubajak@gmail.com>
2024-12-04 18:33:05 -0600

    Created Function fetchCurrentCryptoPrice

---
+++ server/src/controllers/api-controllers/cryptoController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+import { getCurrentCryptoPrice } from "../../services/cryptos/currentCService";
+
+export const fetchCurrentCryptoPrice = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const { id } = req.params;
+
+  try {
+    const currentCryptoPrice = await getCurrentCryptoPrice(id);
+    res.json(currentCryptoPrice);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch current crypto price." });
+  }
+};
