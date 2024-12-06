dc2c07bec052129d1653961d4a432f801e8ce6c0
VanceGoblin <zvkubajak@gmail.com>
2024-12-06 14:57:35 -0600

    Created Route

--- server/src/routes/api/cryptoRoutes.ts
+++ server/src/routes/api/cryptoRoutes.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import {
   fetchCurrentCryptoPrice,
+  fetchHistoricalCryptoPrice,
   fetchCryptoData,
 } from "../../controllers/api-controllers/cryptoController";
 
 const router = Router();
 
-router.get("/price/:id", fetchCurrentCryptoPrice);
+router.get("/current/:id", fetchCurrentCryptoPrice);
+router.get("/historical/:id", fetchHistoricalCryptoPrice);
 router.get("/data/:id", fetchCryptoData);
 
 export { router as cryptoRoutes };
