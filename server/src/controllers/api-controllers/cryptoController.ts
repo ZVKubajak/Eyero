9bcb93b419e1c78e422ff063f931304514a28549
VanceGoblin <zvkubajak@gmail.com>
2024-12-06 15:51:29 -0600

    Removed Current Crypto Price Functions (not needed anymore)

--- server/src/controllers/api-controllers/cryptoController.ts
+++ server/src/controllers/api-controllers/cryptoController.ts
@@ -1,21 +1,6 @@
 import { Request, Response } from "express";
-import { getCurrentCryptoPrice } from "../../services/cryptos/CCurrentService";
 import { getHistoricalCryptoPrice } from "../../services/cryptos/CHistoricalService";
 import { getCryptoData } from "../../services/cryptos/CDataService";
-
-export const fetchCurrentCryptoPrice = async (
-  req: Request,
-  res: Response
-): Promise<void> => {
-  const { id } = req.params;
-
-  try {
-    const currentCryptoPrice = await getCurrentCryptoPrice(id);
-    res.json(currentCryptoPrice);
-  } catch (error) {
-    res.status(500).json({ error: "Failed to fetch current crypto price." });
-  }
-};
 
 export const fetchHistoricalCryptoPrice = async (
   req: Request,
--- server/src/routes/api/cryptoRoutes.ts
+++ server/src/routes/api/cryptoRoutes.ts
@@ -1,13 +1,11 @@
 import { Router } from "express";
 import {
-  fetchCurrentCryptoPrice,
   fetchHistoricalCryptoPrice,
   fetchCryptoData,
 } from "../../controllers/api-controllers/cryptoController";
 
 const router = Router();
 
-router.get("/current/:id", fetchCurrentCryptoPrice);
 router.get("/historical/:id", fetchHistoricalCryptoPrice);
 router.get("/data/:id", fetchCryptoData);
 
