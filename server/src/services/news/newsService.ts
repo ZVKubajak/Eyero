d501971620c30517179e36cf7cc601ca4d851839
bryceberczik <berczikbryce@gmail.com>
2024-12-05 20:18:46 -0600

    added param querys to axios news api fetch, also completed the data we will be fetching for the news apu

--- server/src/services/news/newsService.ts
+++ server/src/services/news/newsService.ts
@@ -1,14 +1,19 @@
 import axios from "axios";
 import { newsapiConfig } from "../../config/config";
-import { describe } from "node:test";
 
 export const getNewsData = async (query: string) => {
     
     try {
         
+        const now = new Date();
+        const sevenDaysAgo = new Date(now.setDate(now.getDate() - 7));
+        const from = sevenDaysAgo.toISOString().split("T")[0];
+
         const response = await axios.get(`${newsapiConfig.baseUrl}/everything`, {
             params: {
                 q: query,
+                from,
+                sortBy: "publishedAt",
                 apikey: newsapiConfig.newsapiApiKey
             }
         });
@@ -20,10 +25,12 @@ export const getNewsData = async (query: string) => {
             name: article.source.name,
             title: article.title,
             description: article.description,
-
+            url: article.url,
+            urlToImage: article.urlToImage,
+            publishedAt: article.publishedAt
         }))
 
-        return n;
+        return newsResults;
     } catch (error) {
         console.error("An error occured:", error)
         throw error;
