b17792149f2e1d6f8b4db8e6b7175c6011b9fda6
VanceGoblin <zvkubajak@gmail.com>
2024-12-08 22:13:03 -0600

    Fine Tuning chart.tsx Styling #2

--- client/src/app/ui/dashboard/Chart/chart.tsx
+++ client/src/app/ui/dashboard/Chart/chart.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import "./chart.css";
 import { Line, LineChart, CartesianGrid } from "recharts";
 import {
   Card,
@@ -15,14 +16,15 @@ import {
   ChartTooltipContent,
 } from "@/components/ui/chart";
 
+// Chart Test Data
 const chartData = [
-  { date: "12/01", price: 250 },
-  { date: "12/02", price: 245 },
-  { date: "12/03", price: 253 },
-  { date: "12/04", price: 257 },
-  { date: "12/05", price: 255 },
-  { date: "12/06", price: 250 },
-  { date: "12/07", price: 261 },
+  { date: "12/01", price: 230 },
+  { date: "12/02", price: 238 },
+  { date: "12/03", price: 222 },
+  { date: "12/04", price: 267 },
+  { date: "12/05", price: 275 },
+  { date: "12/06", price: 281 },
+  { date: "12/07", price: 277 },
 ];
 
 const chartConfig = {
@@ -34,41 +36,43 @@ const chartConfig = {
 
 const Chart = () => {
   return (
-      <Card className="w-1/4 border-slate-500 bg-stone-950">
-        <CardHeader>
-          <CardTitle className="text-white">AAPL</CardTitle>
-          <CardDescription className="text-white text-xl font-bold">$254.09</CardDescription>
-          <CardDescription className="text-slate-500">Stocks</CardDescription>
-        </CardHeader>
-        <CardContent className="h-40 overflow-hidden">
-          <ChartContainer config={chartConfig}>
-            <LineChart
-              width={300}
-              height={120}
-              data={chartData}
-              margin={{
-                top: 10,
-                left: 10,
-                right: 10,
-                bottom: 10,
-              }}
-            >
-              <CartesianGrid vertical={false} stroke="#333" />
-              <ChartTooltip
-                cursor={true}
-                content={<ChartTooltipContent indicator="dot" hideLabel />}
-              />
-              <Line
-                dataKey="price"
-                type="linear"
-                stroke="lightgreen"
-                strokeWidth={2}
-                dot={false}
-              />
-            </LineChart>
-          </ChartContainer>
-        </CardContent>
-      </Card>
+    <Card className="w-1/4 border-slate-800" id="chart-card">
+      <CardHeader>
+        <div style={{ display: "flex" }}>
+          <CardTitle id="chart-card-title">AAPL</CardTitle>
+          <CardDescription id="chart-card-category">Stocks</CardDescription>
+        </div>
+        <CardDescription id="chart-card-price">$254.09</CardDescription>
+      </CardHeader>
+      <CardContent className="h-40 overflow-hidden">
+        <ChartContainer config={chartConfig}>
+          <LineChart
+            width={300}
+            height={120}
+            data={chartData}
+            margin={{
+              top: 10,
+              left: 10,
+              right: 10,
+              bottom: 10,
+            }}
+          >
+            <CartesianGrid vertical={false} stroke="#333" />
+            <ChartTooltip
+              cursor={true}
+              content={<ChartTooltipContent indicator="dot" hideLabel />}
+            />
+            <Line
+              dataKey="price"
+              type="linear"
+              stroke="lightgreen"
+              strokeWidth={2}
+              dot={false}
+            />
+          </LineChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
   );
 };
 
