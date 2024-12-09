97537ff210af86e65013b35e71da457b77e4f292
VanceGoblin <zvkubajak@gmail.com>
2024-12-09 16:40:49 -0600

    Added News Cards to Dashboard to Complete Basic Layout

--- client/src/app/dashboard/(overview)/page.tsx
+++ client/src/app/dashboard/(overview)/page.tsx
@@ -1,17 +1,33 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import HomeChart1 from "@/app/ui/dashboard/Charts/HomeChart1/HomeChart1";
 import HomeChart2 from "@/app/ui/dashboard/Charts/HomeChart2/HomeChart2";
-// Import NewsCard
+import NewsCard from "@/app/ui/dashboard/NewsCard/newsCard";
+import { fetchNews } from "@/lib/newsServices";
+import { InewsItem } from "@/interfaces";
 
 const Page = () => {
+  const [news, setNews] = useState<InewsItem[]>([]);
+
+  useEffect(() => {
+    async function getNews() {
+      const newsData = await fetchNews();
+      setNews(newsData);
+    }
+
+    getNews();
+  }, []);
+
   return (
-    <main className="mx-12 my-8">
+    <main className="mx-12 my-4">
       <div>
         <header>
           <h1 className="text-3xl font-semibold">Trending Markets</h1>
           <h2 className="text-xl">December 8th</h2>
         </header>
         <div>
-          <div className="grid grid-cols-2 mt-8 pb-4 border-b">
+          <div className="grid grid-cols-2 mt-2 pb-4 border-b">
             <HomeChart1 />
             <HomeChart1 />
           </div>
@@ -22,7 +38,23 @@ const Page = () => {
             <HomeChart2 />
           </div>
         </div>
-        {/* News Goes Here */}
+        {news ? (
+          <div className="grid grid-cols-3">
+            {news.slice(0, 3).map((newsItem: InewsItem, index: number) => (
+              <NewsCard
+                key={index}
+                name={newsItem.name}
+                title={newsItem.title}
+                description={newsItem.description}
+                url={newsItem.url}
+                urlToImage={newsItem.urlToImage}
+                publishedAt={new Date(newsItem.publishedAt).toLocaleString()}
+              />
+            ))}
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
       </div>
     </main>
   );
