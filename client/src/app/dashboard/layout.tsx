5369c7ff2180158c77179f60c0d45e5c664153ca
VanceGoblin <zvkubajak@gmail.com>
2024-12-09 08:12:09 -0600

    Created Prototype Layout For Full Page

--- client/src/app/dashboard/layout.tsx
+++ client/src/app/dashboard/layout.tsx
@@ -4,12 +4,22 @@ import Header from "../ui/Header/header";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <div>
-        <Header />
+      <div className="flex flex-col h-screen">
+        <header className="flex justify-end h-20 items-center border-b-2">
+          <div className="w-4/5">
+            <Header />
+          </div>
+        </header>
+
+        <main className="flex flex-1">
+          <div className=" flex flex-col w-1/5 border-r-2">
+            <div>Sidebar</div>
+          </div>
+          <div className="flex-1 border-l-2">{children}</div>
+        </main>
       </div>
-      <div>{children}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
