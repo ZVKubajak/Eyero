b985bb183cb0db5bfc06308f71cc62d4f89be167
VanceGoblin <zvkubajak@gmail.com>
2024-12-10 09:13:05 -0600

    Turned transformStockData Into Its Own Function For Scalability

--- client/src/lib/homeData.ts
+++ client/src/lib/homeData.ts
@@ -1,23 +1,9 @@
+import { transformStockData } from "./utils";
 import { StockData } from "@/interfaces";
 
 export const fetchHomepageStockData = async (
   ticker: string
 ): Promise<StockData[]> => {
-  const transformStockData = (data: any): StockData[] => {
-    const timeSeries = data["Time Series (Daily)"];
-
-    if (!timeSeries) return [];
-
-    return Object.entries(timeSeries).map(([date, values]: [string, any]) => ({
-      date,
-      open: parseFloat(values["1. open"]),
-      high: parseFloat(values["2. high"]),
-      low: parseFloat(values["3. low"]),
-      close: parseFloat(values["4. close"]),
-      volume: parseInt(values["5. volume"], 10),
-    }));
-  };
-
   try {
     const response = await fetch(`/routes/stocks/home/${ticker}`);
 
--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -1,6 +1,22 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+import { StockData } from "@/interfaces";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const transformStockData = (data: any): StockData[] => {
+  const timeSeries = data["Time Series (Daily)"];
+
+  if (!timeSeries) return [];
+
+  return Object.entries(timeSeries).map(([date, values]: [string, any]) => ({
+    date,
+    open: parseFloat(values["1. open"]),
+    high: parseFloat(values["2. high"]),
+    low: parseFloat(values["3. low"]),
+    close: parseFloat(values["4. close"]),
+    volume: parseInt(values["5. volume"], 10),
+  }));
+};
