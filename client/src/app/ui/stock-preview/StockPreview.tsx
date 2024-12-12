8ef7dc13d35dba6028f58edd8e05bb6395902866
VanceGoblin <zvkubajak@gmail.com>
2024-12-12 10:30:44 -0600

    Added if Block to Handle Logic Depending on Parameter

--- client/src/app/ui/stock-preview/StockPreview.tsx
+++ client/src/app/ui/stock-preview/StockPreview.tsx
@@ -1,7 +1,82 @@
+"use client";
+
 import "./stockpreview.css";
+import {
+  fetchHomepageStockData,
+  fetchHomepageCryptoData,
+} from "@/lib/homeData";
+import { StockData } from "@/interfaces";
+import { Area, AreaChart, CartesianGrid, XAxis, YAxis } from "recharts";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { useState, useEffect } from "react";
 import StockGraph from "../../images/images.jpeg";
 
-const StockPreview = () => {
+type StockPreviewProps = { ticker: string } | { id: string };
+
+const chartConfig = {
+  price: {
+    label: "Price",
+    color: "hsl(var(--chart1))",
+  },
+} satisfies ChartConfig;
+
+const StockPreview = (props: StockPreviewProps) => {
+  const [stockChartData, setStockChartData] = useState<
+    { date: string; price: number }[]
+  >([]);
+  const [cryptoChartData, setCryptoChartData] = useState<
+    { timestamp: Number; price: number }[]
+  >([]);
+
+  if ("ticker" in props) {
+    useEffect(() => {
+      const fetchData = async () => {
+        const data = await fetchHomepageStockData(props.ticker);
+
+        if (data) {
+          const mappedData = data
+            .map((dataPoint: StockData) => ({
+              date: dataPoint.date,
+              price: dataPoint.close,
+            }))
+            .reverse()
+            .slice(-30);
+          setStockChartData(mappedData);
+        }
+      };
+
+      fetchData();
+    }, []);
+  } else if ("id" in props) {
+    useEffect(() => {
+      const fetchData = async () => {
+        const data = await fetchHomepageCryptoData(props.id);
+
+        if (data) {
+          const mappedData = data.prices.map((dataPoint: [number, number]) => ({
+            timestamp: dataPoint[0],
+            price: dataPoint[1],
+          }));
+          setCryptoChartData(mappedData);
+        }
+      };
+
+      fetchData();
+    }, []);
+  }
+
   const stockName = "Dow Jones Industrial Accositoion!";
   const maxLength = 22;
 
@@ -18,12 +93,12 @@ const StockPreview = () => {
         <h2>{truncateText(stockName, maxLength)}</h2>
       </div>
       <div className="stock-preview-graph">
-        <img src={StockGraph.src} alt="example stock graph" width={60}/>
+        <img src={StockGraph.src} alt="example stock graph" width={60} />
       </div>
       <div className="stock-preview-prices">
         <h1>44,401.93</h1>
         <div className="change-holder">
-            <h2>-240.59</h2>
+          <h2>-240.59</h2>
         </div>
       </div>
     </div>
